5b0e001a7ab70832584e09139374887b0c5265a0
Kacper Reszka <kapiresz@gmail.com>
2022-11-09 18:08:50 +0100

    first step

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: const Scaffold(
+        body: Text('siema'),
+      ),
     );
   }
 }
