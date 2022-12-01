bea084b37cd9e08793344780b98b28b2ccfe17ee
Kacper Reszka <kapiresz@gmail.com>
2022-12-01 17:42:33 +0100

    Implement adding restaurants

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,7 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'add_opinion/add_opinion_page_content.dart';
+import 'my_account/my_account_page_content.dart';
+import 'restaurants/restaurants_page_content.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -46,98 +47,6 @@ class _HomePageState extends State<HomePage> {
               icon: Icon(Icons.person), label: 'Moje konto'),
         ],
       ),
-    );
-  }
-}
-
-class MyAccountPageContent extends StatelessWidget {
-  const MyAccountPageContent({
-    Key? key,
-    required this.email,
-  }) : super(key: key);
-
-  final String? email;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Jesteś zalogowany jako $email'),
-          const SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            child: const Text('Wyloguj'),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class AddOpinionPageContent extends StatelessWidget {
-  const AddOpinionPageContent({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dwa'),
-    );
-  }
-}
-
-class RestaurantsPageContent extends StatelessWidget {
-  const RestaurantsPageContent({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-      stream: FirebaseFirestore.instance.collection('restaurants').snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return const Center(child: Text('Coś poszło nie tak'));
-        }
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: Text('Trwa ładowanie danych'));
-        }
-
-        final documents = snapshot.data!.docs;
-
-        return ListView(
-          children: [
-            for (final document in documents) ...[
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(document['name']),
-                        Text(document['Danie']),
-                      ],
-                    ),
-                    Text(document['rating'].toString()),
-                  ],
-                ),
-              )
-            ],
-            const Scaffold(
-              body: Text('Nie ma żadnych restauracji'),
-            ),
-          ],
-        );
-      },
     );
   }
 }
