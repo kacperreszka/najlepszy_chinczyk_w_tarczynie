8ab910e43d02d05ea9de9c47b3e5c7e6cf727509
Kacper Reszka <kapiresz@gmail.com>
2022-12-14 17:03:27 +0100

    Create New State

--- lib/app/restaurants/cubit/restaurants_cubit.dart
+++ lib/app/restaurants/cubit/restaurants_cubit.dart
@@ -1,8 +1,10 @@
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 part 'restaurants_state.dart';
 
 class RestaurantsCubit extends Cubit<RestaurantsState> {
-  RestaurantsCubit() : super(RestaurantsState());
+  RestaurantsCubit() : super(RestaurantsState(documents: const []));
 }
