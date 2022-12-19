539268dc6fbacf0c9e11efedd6b0cfce6b3646a0
Kacper Reszka <kapiresz@gmail.com>
2022-12-19 19:45:15 +0100

    1 Step to state data loading

--- lib/app/restaurants/restaurants_page_content.dart
+++ lib/app/restaurants/restaurants_page_content.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'cubit/restaurants_cubit.dart';
 
 class RestaurantsPageContent extends StatelessWidget {
   const RestaurantsPageContent({
@@ -8,44 +11,54 @@ class RestaurantsPageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-      stream: FirebaseFirestore.instance.collection('restaurants').snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return const Center(child: Text('Coś poszło nie tak'));
-        }
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: Text('Trwa ładowanie danych'));
-        }
+    return BlocProvider(
+      create: (context) => RestaurantsCubit(),
+      child: BlocBuilder<RestaurantsCubit, RestaurantsState>(
+        builder: (context, state) {
+          if (state.erroMessage.isNotEmpty) {
+            return Center(
+                child: Text('Coś poszło nie tak: ${state.erroMessage}'));
+          }
+          if (state.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
-        final documents = snapshot.data!.docs;
+          final documents = state.documents;
 
-        return ListView(
-          children: [
-            for (final document in documents) ...[
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(document['name']),
-                        Text(document['Danie']),
-                      ],
-                    ),
-                    Text(document['rating'].toString()),
-                  ],
-                ),
-              )
+          return ListView(
+            children: [
+              for (final document in documents) ...[
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(document['name']),
+                          Text(document['Danie']),
+                        ],
+                      ),
+                      Text(document['rating'].toString()),
+                    ],
+                  ),
+                )
+              ],
+              const Scaffold(
+                body: Text('Nie ma żadnych restauracji'),
+              ),
             ],
-            const Scaffold(
-              body: Text('Nie ma żadnych restauracji'),
-            ),
-          ],
-        );
-      },
+          );
+
+          return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+            stream: FirebaseFirestore.instance
+                .collection('restaurants')
+                .snapshots(),
+            builder: (context, snapshot) {},
+          );
+        },
+      ),
     );
   }
 }
